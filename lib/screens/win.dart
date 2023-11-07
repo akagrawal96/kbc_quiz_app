efee99a45b5f4f97776cc48c93917fbb1763e4da
akagrawal96 <agrawalsirj@gmail.com>
2023-11-07 07:07:44 +0530

    day3 work - design win page with konfetti animation particals

---
+++ lib/screens/win.dart
@@ -0,0 +1,103 @@
+import 'package:confetti/confetti.dart';
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+
+class Win extends StatefulWidget {
+  const Win({super.key});
+
+  @override
+  State<Win> createState() => _WinState();
+}
+
+class _WinState extends State<Win> {
+  late ConfettiController confettiController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    setState(() {
+      initController();
+    });
+    confettiController.play();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(image: AssetImage("assets/images/win.png"),
+        fit: BoxFit.cover)
+      ),
+      child: Scaffold(
+        floatingActionButton: ElevatedButton(
+          child: Row(
+mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(Icons.share),
+              SizedBox(width: 10,),
+              Text("Share with Friends"),
+            ],
+          ),onPressed: (){},
+        ),
+    backgroundColor: Colors.transparent,
+        body: Container(
+          child:
+          Stack(
+            children: [
+              Container(
+                child: Column(mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Congratulation !",
+                      style: TextStyle(fontSize: 35,fontWeight: FontWeight.bold),),
+                    Text("Your ANSWER IS CORRECT",
+                      style: TextStyle(fontSize: 17,fontWeight: FontWeight.w500),),
+                    SizedBox(height: 15,),
+                    Text("You won",
+                      style: TextStyle(fontSize: 15,fontWeight: FontWeight.w400),),
+                    Text("Rs. 5,40,000",
+                      style: TextStyle(fontSize: 28,fontWeight: FontWeight.w700),),
+                    SizedBox(height: 25,),
+                    Container(
+                        padding: EdgeInsets.all(20),
+                        child: Image.asset("assets/images/cheque.jpg"),
+                    ),
+                    ElevatedButton(onPressed: (){},
+                        child: Text("Next"))
+
+
+                  ],
+                ),
+              ),
+              buildConfettiWidget(confettiController, pi/2)
+            ],
+
+          ),
+        ),
+      ),
+    );
+  }
+
+
+
+  Align buildConfettiWidget(controller,double blastDirection){
+    return Align(alignment: Alignment.topCenter,
+    child: ConfettiWidget(
+      maximumSize: Size(30,30),
+      shouldLoop: false,
+      confettiController: controller,
+      blastDirection: blastDirection,
+      blastDirectionality: BlastDirectionality.explosive,
+      maxBlastForce: 20,
+      minBlastForce: 8,
+      emissionFrequency: 0.02,
+      numberOfParticles: 8,
+      gravity: 0.6,
+    ),);
+  }
+
+  void initController() {
+    confettiController = ConfettiController(duration: const Duration(seconds: 1));
+  }
+}
